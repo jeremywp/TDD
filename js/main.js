d3debd3ef229eaff4f951aac360c6fc1ca8bda06
jeremywp <dr.jpratt@gmail.com>
2018-11-17 23:34:35 -0700

    Lots of new stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let courseList;
 let numholes = 18;
 let globaltee;
+let numplayers = 0;
+let playernames = [];
 
 (function() {
     onDocumentLoad();
@@ -44,71 +46,267 @@ function loadCourse(courseid) {
 
 function chooseTee(teevalue) {
     globaltee = teevalue;
-
-    buildCard();
+    return globaltee;
 }
 
 function buildCard() {
-    for(let i = 0; i < numholes; i++) {
-        $(".right").append("<div id='column" + i + "' class='column'>" +
+    $(".content").css("display", "flex");
+
+    $(".left").append("<div class='row-title'>Hole # </div>" +
+        "<div class='row-title'>Yards </div>" +
+        "<div class='row-title'>Handicap </div>" +
+        "<div class='par row-title'>Par </div>");
+
+    for(let i = 0; i < 9; i++) {
+        $(".out").append("<div id='column" + i + "' class='column'>" +
             "<span>"+ (i+1) +"</span>" +
-            "<div>" + mycourse.data.holes[i].teeBoxes[globaltee].yards + "</div>" +
-            "<div>" + mycourse.data.holes[i].teeBoxes[globaltee].hcp + "</div>" +
-            "<div>" + mycourse.data.holes[i].teeBoxes[globaltee].par + "</div>" +
+            "<div class='row-data'>" + mycourse.data.holes[i].teeBoxes[globaltee].yards + "</div>" +
+            "<div class='row-data'>" + mycourse.data.holes[i].teeBoxes[globaltee].hcp + "</div>" +
+            "<div class='row-data'>" + mycourse.data.holes[i].teeBoxes[globaltee].par + "</div>" +
             "</div>");
     }
-    addHoles();
+
+    let outyards = 0;
+    let outhcp = 0;
+    let outpar = 0;
+
+    for(let i = 0; i < 9; i++) {
+        outyards += mycourse.data.holes[i].teeBoxes[globaltee].yards;
+        outhcp += mycourse.data.holes[i].teeBoxes[globaltee].hcp;
+        outpar += mycourse.data.holes[i].teeBoxes[globaltee].par
+    }
+
+    $(".out").append("<div id='outcolumn' class='column'>" +
+        "<span>OUT</span>" +
+        "<div class='row-data'>" + outyards + "</div>" +
+        "<div class='row-data'>" + outhcp + "</div>" +
+        "<div class='row-data'>" + outpar + "</div>" +
+        "</div>");
+
+    for(let i = 10; i < numholes; i++) {
+        $(".in").append("<div id='column" + i + "' class='column'>" +
+            "<span>"+ (i+1) +"</span>" +
+            "<div class='row-data'>" + mycourse.data.holes[i].teeBoxes[globaltee].yards + "</div>" +
+            "<div class='row-data'>" + mycourse.data.holes[i].teeBoxes[globaltee].hcp + "</div>" +
+            "<div class='row-data'>" + mycourse.data.holes[i].teeBoxes[globaltee].par + "</div>" +
+            "</div>");
+    }
+
+    let inyards = 0;
+    let inhcp = 0;
+    let inpar = 0;
+
+    for(let i = 10; i < 18; i++) {
+        inyards += mycourse.data.holes[i].teeBoxes[globaltee].yards;
+        inhcp += mycourse.data.holes[i].teeBoxes[globaltee].hcp;
+        inpar += mycourse.data.holes[i].teeBoxes[globaltee].par
+    }
+
+    $(".in").append("<div id='incolumn' class='column'>" +
+        "<span>IN</span>" +
+        "<div class='row-data'>" + inyards + "</div>" +
+        "<div class='row-data'>" + inhcp + "</div>" +
+        "<div class='row-data'>" + inpar + "</div>" +
+        "</div>");
+
+    let totalyards = inyards + outyards;
+    let totalhcp = inhcp + outhcp;
+    let totalpar = inpar + outpar;
+
+    $(".total").append("<div id='totalcolumn' class='column'>" +
+        "<span>TOTAL</span>" +
+        "<div class='row-data'>" + totalyards + "</div>" +
+        "<div class='row-data'>" + totalhcp + "</div>" +
+        "<div class='row-data' id='totalpar'>" + totalpar + "</div>" +
+        "</div>");
+
+    $(".modal-background").hide();
+    $(".content").css("filter", "blur(0px");
 }
 
 function addHoles() {
-    for(let p = 1; p <= $("#numberOfPlayers").val(); p++) {
-        for (let h = 0; h < numholes; h++) {
-            $("#column" + h).append("<input type='number' id='p"+ p +"h"+ (h+1) +"'>");
+    let p = numplayers;
+        for (let h = 0; h < 9; h++) {
+            $("#column" + h).append("<input class='outscoreplayer"+ p +"' type='number' id='p"+ p +"h"+ (h+1) +"'>");
         }
+
+        $("#outcolumn").append("<input class='scoreplayer' id='totalout"+ p +"' type='number' readonly>");
+
+        for (let h = 10; h < 18; h++) {
+            $("#column" + h).append("<input class='inscoreplayer"+ p +"' type='number' id='p"+ p +"h"+ (h+1) +"'>");
+        }
+
+        $("#incolumn").append("<input class='scoreplayer' id='totalin"+ p +"' type='number' readonly>");
+
+        $("#totalcolumn").append("<input class='scoreplayer' id='total"+ p +"' type='number' readonly>");
+    }
+
+function addPlayer() {
+    numplayers++;
+    if (numplayers <= 4) {
+        $(".left").append("<div class='player' id='p" + numplayers + "'><span class='playername'>" + $(".pname").val() +
+            "</span></div>");
+        addHoles();
+    }
+    else if (numplayers > 4){
+        $(".perror-box").css("display", "flex");
+        $(".perror").html("Sorry, no more than four players.");
+    }
+
+    $(".pname").val("");
+    return numplayers;
+}
+
+function checkName () {
+    let name = $(".pname").val();
+    if (playernames.includes(name)) {
+        $(".perror-box").show();
+        $(".perror").html("Sorry, that name is in use.");
+    }
+    else {
+        playernames.push($(".pname").val());
+        $(".perror-box").hide();
+        addPlayer();
     }
 }
 
-function addScore(myid) {
-    let myscore = 0;
-    //parse the player number out of the id, make that p
-    for(let i = 0; i <= numholes; i++) {
-        let scoreitem = $("#p" + p + "h" + i).val();
-        myscore += scoreitem;
-    }
-    return myscore;
-}
-
-addPlayers = () => {
-    let totalPlayers = $("#numberOfPlayers").val();
-
-    $(".left").append("<div>Hole # </div>" +
-        "<div>Yards </div>" +
-        "<div>Handicap </div>" +
-        "<div>Par</div>");
-    for(let i = 1; i <= totalPlayers; i++) {
-        $(".left").append("<div class='player'>Player "+ i +
-                "<button class='playerDelete'>" +
-                    "<i class=\"far fa-trash-alt\"></i>" +
-                "</button>" +
-            "</div>");
-    }
-    $(".modal-background").hide();
-    $(".content").css("filter", "blur(0px");
-
-    $(".playerDelete").click(function() {
-        $(".this").closest().remove();
+$(document).on("change", ".outscoreplayer1", function () {
+    let sumout = 0;
+    $(".outscoreplayer1").each(function () {
+        sumout += +$(this).val();
     });
-};
+    $("#totalout1").val(sumout);
+});
 
-
-function checkname (myval) {
-    $(".pname").each(function (){
-        let player = $(this).html();
-        if(myval === player){
-            $(".perror").html("Sorry, that name is in use.");
-        }
-        else if(myval !== player){
-            $(".perror").html("Great name!");
-        }
+$(document).on("change", ".inscoreplayer1", function () {
+    let sumin = 0;
+    $(".inscoreplayer1").each(function () {
+        sumin += +$(this).val();
     });
-}
+    $("#totalin1").val(sumin);
+});
+
+$(document).on("change", ".outscoreplayer1", function () {
+
+    let sumtotal = $("#totalout1").val() + $("#totalin1").val();
+
+    $("#total1").val(sumtotal);
+});
+
+$(document).on("change", ".inscoreplayer1", function () {
+
+    let sumtotal = parseInt($("#totalout1").val()) + parseInt($("#totalin1").val());
+
+    $("#total1").val(sumtotal);
+});
+
+
+
+$(document).on("change", ".outscoreplayer2", function () {
+    let sumout = 0;
+    $(".outscoreplayer2").each(function () {
+        sumout += +$(this).val();
+    });
+    $("#totalout2").val(sumout);
+});
+
+$(document).on("change", ".inscoreplayer2", function () {
+    let sumin = 0;
+    $(".inscoreplayer2").each(function () {
+        sumin += +$(this).val();
+    });
+    $("#totalin2").val(sumin);
+});
+
+$(document).on("change", ".outscoreplayer2", function () {
+
+    let sumtotal = $("#totalout2").val() + $("#totalin2").val();
+
+    $("#total2").val(sumtotal);
+});
+
+$(document).on("change", ".inscoreplayer2", function () {
+
+    let sumtotal = parseInt($("#totalout2").val()) + parseInt($("#totalin2").val());
+
+    $("#total2").val(sumtotal);
+});
+
+
+
+$(document).on("change", ".outscoreplayer3", function () {
+    let sumout = 0;
+    $(".outscoreplayer3").each(function () {
+        sumout += +$(this).val();
+    });
+    $("#totalout3").val(sumout);
+});
+
+$(document).on("change", ".inscoreplayer3", function () {
+    let sumin = 0;
+    $(".inscoreplayer3").each(function () {
+        sumin += +$(this).val();
+    });
+    $("#totalin3").val(sumin);
+});
+
+$(document).on("change", ".outscoreplayer3", function () {
+
+    let sumtotal = $("#totalout3").val() + $("#totalin3").val();
+
+    $("#total3").val(sumtotal);
+});
+
+$(document).on("change", ".inscoreplayer3", function () {
+
+    let sumtotal = parseInt($("#totalout3").val()) + parseInt($("#totalin3").val());
+
+    $("#total3").val(sumtotal);
+});
+
+
+$(document).on("change", ".outscoreplayer4", function () {
+    let sumout = 0;
+    $(".outscoreplayer4").each(function () {
+        sumout += +$(this).val();
+    });
+    $("#totalout4").val(sumout);
+});
+
+$(document).on("change", ".inscoreplayer4", function () {
+    let sumin = 0;
+    $(".inscoreplayer4").each(function () {
+        sumin += +$(this).val();
+    });
+    $("#totalin4").val(sumin);
+});
+
+$(document).on("change", ".outscoreplayer4", function () {
+
+    let sumtotal = $("#totalout4").val() + $("#totalin4").val();
+
+    $("#total4").val(sumtotal);
+});
+
+$(document).on("change", ".inscoreplayer4", function () {
+
+    let sumtotal = parseInt($("#totalout4").val()) + parseInt($("#totalin4").val());
+
+    $("#total4").val(sumtotal);
+});
+
+$(document).on("change", "#p1h18", function () {
+    let totalscore = parseInt($("#total1").val());
+    let totalpar = parseInt($("#totalpar").val());
+    let totaldif = totalscore - totalpar;
+
+    if (totalscore > totalpar){
+        alert("You're score was \+" + totaldif + "over par.  Better luck next time!");
+    }
+    else if (totalscore < totalpar) {
+        alert("You're score was " + totaldif + "over par.  Great job!");
+    }
+    else {
+        alert("You score was on par.  Not bad!");
+    }
+});
